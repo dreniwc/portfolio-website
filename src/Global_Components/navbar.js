2157340b726e59ab6c78b66c2104d915516b1c7c
Callum Dreniw <callum.dreniw@iprospect.com>
2020-06-09 17:00:35 +0100

    fixing exact on navbar showing active on home constant

--- src/Global_Components/navbar.js
+++ src/Global_Components/navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <nav role="navigation" id="skiptonav">
             <ul>
-                <li><NavLink to='/'>Home</NavLink></li>
+                <li><NavLink to='/' exact>Home</NavLink></li>
                 <li><NavLink to='/about'>About</NavLink></li>
                 <li><NavLink to='/portfolio'>Portfolio</NavLink></li>
                 <li><NavLink to='/contact'>Contact</NavLink></li>
