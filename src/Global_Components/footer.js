a495c303b2b172f7f754347ac9e10f3321cec7da
Callum Dreniw <callum.dreniw@iprospect.com>
2020-06-10 21:34:28 +0100

    updating homepage section styles for grid layout

--- src/Global_Components/footer.js
+++ src/Global_Components/footer.js
@@ -3,15 +3,8 @@ import React from 'react';
 function Footer() {
     return (
         <footer>
-            <div className="container">
-                <div className="contact Info">
-                    <p>Contact Info here</p>
-                </div>
-                <div className="copyright">
-                    <p>&copy; {(new Date().getFullYear())} Callum Dreniw - All Rights Reserved </p>
-                </div>
-            </div>
-        </footer>
+            <p>&copy; {(new Date().getFullYear())} Callum Dreniw - All Rights Reserved </p>
+        </footer >
     )
 }
 
