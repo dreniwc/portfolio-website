9eea64c871f71895673fd27c6269c9362080d4f0
Callum Dreniw <callum.dreniw@iprospect.com>
2020-06-12 22:44:33 +0100

    adapting svgs in dark mode

--- src/Global_Components/Themes.js
+++ src/Global_Components/Themes.js
@@ -3,10 +3,16 @@ export const lightTheme = {
     text: '#363537',
     toggleBorder: '#FFF',
     background: '#363537',
+    secondaryBackground: '#005a6a',
+    secondaryText: '#FFFFFF',
+    svgBlack: '#000',
 }
 export const darkTheme = {
     body: '#363537',
     text: '#FAFAFA',
     toggleBorder: '#6B8096',
     background: '#999',
+    secondaryBackground: '#004552',
+    secondaryText: '#FFFFFF',
+    svgBlack: '#FFFFFF',
 }
--- src/Global_Components/globalStyles.js
+++ src/Global_Components/globalStyles.js
@@ -6,4 +6,13 @@ export const GlobalStyles = createGlobalStyle`
     font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
     transition: all 0.50s linear;
   }
+  .quickIntro {
+    background: ${({ theme }) => theme.secondaryBackground};
+    color: ${({ theme }) => theme.secondaryText};
+    transition: all 0.50s linear;
+  }
+  svg.logoBlack {
+    fill: ${({ theme }) => theme.svgBlack};
+    stroke: ${({ theme }) => theme.svgBlack};
+}
   `
