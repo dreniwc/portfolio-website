37d3fb951efdc39a96efad8e41d01f5be2262bda
Callum Dreniw <callum.dreniw@iprospect.com>
2020-06-12 22:14:49 +0100

    inital start to dark mode;

--- src/Global_Components/App.js
+++ src/Global_Components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import '../Styles/app.scss'
 import Header from "./header"
 import { Switch, Route } from 'react-router-dom';
@@ -8,32 +8,48 @@ import {
   TransitionGroup,
 } from 'react-transition-group';
 
+import { ThemeProvider } from "styled-components";
+import  {useDarkMode} from "../Global_Components/useDarkMode"
+import { GlobalStyles } from "../Global_Components/globalStyles";
+import { lightTheme, darkTheme } from "../Global_Components/Themes"
+import Toggle from "../Global_Components/Toggler"
+
+
 import Home from '../pages/Home';
 import About from '../pages/about/about';
 import Portfolio from '../pages/portfolio/portfolio';
 import Contact from '../pages/contact/Contact';
 
 function App() {
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
+
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
   return (
-    <div className="App">
-      <Header />
-      <Route render={({ location }) => (
-        <TransitionGroup>
-          <CSSTransition
-            key={location.key}
-            timeout={300}
-            classNames="fade"
-          >
-            <Switch location={location}> {/* The Switch decides which component to show based on the current URL.*/}
-              <Route exact path='/' component={Home}></Route>
-              <Route exact path='/about' component={About}></Route>
-              <Route exact path='/portfolio' component={Portfolio}></Route>
-              <Route exact path='/contact' component={Contact}></Route>
-            </Switch>
-          </CSSTransition>
-        </TransitionGroup>
-      )} />
-    </div>
+    <ThemeProvider theme={themeMode}>
+      <>
+        <GlobalStyles />
+        <div className="App">
+        <Toggle theme={theme} toggleTheme={themeToggler} />
+          <Header />
+          <Route render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition
+                key={location.key}
+                timeout={300}
+                classNames="fade"
+              >
+                <Switch location={location}> {/* The Switch decides which component to show based on the current URL.*/}
+                  <Route exact path='/' component={Home}></Route>
+                  <Route exact path='/about' component={About}></Route>
+                  <Route exact path='/portfolio' component={Portfolio}></Route>
+                  <Route exact path='/contact' component={Contact}></Route>
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )} />
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
 
