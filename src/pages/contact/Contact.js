af2837218018f1e42619cf60d7c7cf24221aa655
Callum Dreniw <callum.dreniw@iprospect.com>
2020-06-15 21:13:58 +0100

    adding ajax form

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -2,37 +2,92 @@ import React from 'react';
 
 import Footer from "../../Global_Components/footer"
 
-function Contact() {
-    return (
-        <div className="wrapper">
-            <main role="main" id="skipnav">
-
-                <div className="hero_outerContainer">
-                    <div className="container">
-                        <div className="page">
-                            <p>Contact</p>
-                            <form name="contact" method="POST" data-netlify="true" action="/thank-you">
-                                <input type="hidden" name="form-name" value="contact" />
-                                <p>
-                                    <label>Your Name: <input type="text" name="name" required /></label>
-                                </p>
-                                <p>
-                                    <label>Your Email: <input type="email" name="email" required /></label>
-                                </p>
-                                <p>
-                                    <label>Message: <textarea name="message" required></textarea></label>
-                                </p>
-                                <p>
-                                    <button type="submit">Send</button>
-                                </p>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </main>
-            <Footer />
-        </div>
-    )
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
 }
+
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: "", email: "", message: "" };
+    }
+
+    /* Here’s the juicy bit for posting the form submission */
+
+    handleSubmit = e => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...this.state })
+        })
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+
+        e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+        const { name, email, message } = this.state;
+        return (
+            <form onSubmit={this.handleSubmit} data-netlify="true">
+                <p>
+                    <label>
+                        Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Message: <textarea name="message" value={message} onChange={this.handleChange} />
+                    </label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
+        );
+    }
+}
+
+// function Contact() {
+//     return (
+//         <div className="wrapper">
+//             <main role="main" id="skipnav">
+
+//                 <div className="hero_outerContainer">
+//                     <div className="container">
+//                         <div className="page">
+//                             <p>Contact</p>
+//                             <form name="contact" method="POST" data-netlify="true" action="/thank-you">
+//                                 <input type="hidden" name="form-name" value="contact" />
+//                                 <p>
+//                                     <label>Your Name: <input type="text" name="name" required /></label>
+//                                 </p>
+//                                 <p>
+//                                     <label>Your Email: <input type="email" name="email" required /></label>
+//                                 </p>
+//                                 <p>
+//                                     <label>Message: <textarea name="message" required></textarea></label>
+//                                 </p>
+//                                 <p>
+//                                     <button type="submit">Send</button>
+//                                 </p>
+//                             </form>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </main>
+//             <Footer />
+//         </div>
+//     )
+// }
 
 export default Contact;
