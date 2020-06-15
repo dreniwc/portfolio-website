b06deb8d748e525153668f6a735eeb90ec787b8a
Callum Dreniw <callum.dreniw@iprospect.com>
2020-06-15 18:10:09 +0100

    updating home to use only json data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,12 +30,10 @@ function Home({ user }) {
                         <div className="quickIntro">
                             <div className="container">
                                 <div className="homeIntro">
-                                    <p>Hi, I'm Callum Dreniw.</p>
-                                    <p>I am a highly motivated member of the Conversion Rate Industry with a keen interest in solving problems. I relish the opportunity to learn new methods, techniques, and skills in and around my line of work. Settling for nothing less than the highest standard of work is imperative and engaging my initiative with any resources provided is of second nature to me.</p>
-
-                                    <p>I conduct vast amounts of A/B tests for a variety of the UK’s biggest companies in multiple markets; all aiming to improve their Key Performance Indicators.</p>
-
-                                    <p>User Experience and Data Analytics are of the utmost importance relating to the optimisation of the client’s websites; resulting in a high standard of efficiency and effectiveness throughout the working process.</p>
+                                    <p>{user.about.into}</p>
+                                    <p>{user.about.line1}</p>
+                                    <p>{user.about.line2}</p>
+                                    <p>{user.about.line3}</p>
                                 </div>
                             </div>
                         </div>
