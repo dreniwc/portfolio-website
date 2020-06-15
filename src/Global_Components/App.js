415b2c21744191aab368b75f6e918726c78e0b8d
Callum Dreniw <callum.dreniw@iprospect.com>
2020-06-15 20:41:34 +0100

    adding no match use location in switch

--- src/Global_Components/App.js
+++ src/Global_Components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import '../Styles/app.scss'
 import Header from "./header"
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useLocation } from 'react-router-dom';
 
 import {
   CSSTransition,
@@ -27,6 +27,17 @@ function App() {
 
   const themeMode = theme === 'light' ? lightTheme : darkTheme;
   if(!mountedComponent) return <div/>
+  function NoMatch() {
+    let location = useLocation();
+  
+    return (
+      <div>
+        <h3>
+          No match for <code>{location.pathname}</code>
+        </h3>
+      </div>
+    );
+  }
   return (
     <ThemeProvider theme={themeMode}>
       <>
@@ -45,6 +56,7 @@ function App() {
                   <Route exact path='/' component={() => (<Home user={user} />)}></Route>
                   <Route path='/about' component={() => (<About user={user} />)} ></Route>
                   <Route path='/contact' component={Contact} ></Route>
+                  <Route component={NoMatch} />
                 </Switch>
               </CSSTransition>
             </TransitionGroup>
